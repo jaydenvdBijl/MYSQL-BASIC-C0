69cecca9ab4b46439edb401de80feab1f2d2cd83
jayden van der bijl <2158035@talnet.nl>
2022-02-14 13:18:38 +0100

    opdracht7

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,14 @@
 -- Opdracht 1
-
+DELETE FROM `players` WHERE id = 198329;
+DELETE FROM `players` WHERE id = 199304;
+DELETE FROM `players` WHERE id = 207494;
+DELETE FROM `players` WHERE id = 226753;
 -- Opdracht 2
 
 -- Opdracht 3
+
+
+198329
+199304
+207494
+226753

9ed64df3366fa62f37a4de499a677a130aeab9b0
jayden van der bijl <2158035@talnet.nl>
2022-02-14 12:39:40 +0100

    opdracht6

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM `players` WHERE name = "david silva";
 -- Opdracht 2 
-
+DELETE FROM `players` WHERE club = "Willem II";
 -- Opdracht 3
-
+DELETE FROM `players` WHERE club = "FC Barcelona" AND nationality = "Belgium";
 -- Opdracht 4
-
+DELETE FROM `players` WHERE age >= 38;
 -- Opdracht 5
+DELETE FROM `players` WHERE id = 167905 OR 169595;
